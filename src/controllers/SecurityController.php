e8b0d63ea0c2b65a1e238959b60f51e268a471b0
KasiaOT <kasia.ostoia@gmail.com>
2025-11-05 15:23:02 +0100

    Lab6

--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -3,6 +3,7 @@
 require_once 'AppController.php'; // załączenie AppControllera
 class SecurityController extends AppController {
 
+    // TODO dekarator, który definiuje, jakie metody HTTP są dostępne
     public function login() {
         // TODO
         // pobieramy z formularza email, haslo
@@ -10,11 +11,49 @@ class SecurityController extends AppController {
         // jesli istnieje przekierowujemy na dashboard
 
         //return $this->render("login");
-        return $this->render("login", ['messages' => 'blad logowania']);
+        //return $this->render("login", ['messages' => 'blad logowania']);
+
+        //if($this->isGet()) {
+        //    return $this->render("login");
+        //} 
+        if($this->isPost()) {
+            return $this->render("login");
+        } 
+        //var_dump(IS_POST);
+
+        if ($email === ' ') {
+            return $this->render("login", ["message" => "Podaj email"]);
+        }
+
+        $email = $_POST["email"] ?? '';
+        $password = $_POST["password"] ?? '';
+        var_dump($email, $password);
+        $this->render("dashboard");
+
     }
 
     public function register() {
+        // TODO pobranie z formularza email i hasła
+        // TODO insert do bazy danych
+        // TODO zwrocenie informajci o pomyslnym zarejstrowaniu
 
-        return $this->render("register");
+        //return $this->render("register");
+
+        if ($this->isGet()) {
+            return $this->render("register");
+        }
+
+        if($this->isPost()) {
+            return $this->render("register");
+        }
+
+        $email = $_POST["email"] ?? '';
+        $password1 = $_POST["password1"] ?? '';
+        $password2 = $_POST["password2"] ?? '';
+        $firstname = $_POST["firstname"] ?? '';
+        $lastname = $_POST["lastname"] ?? '';
+
+        return $this->render("login", ["message" => "Zarejestrowano uytkownika ".$email]);
+
     }
 }
